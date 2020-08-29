eef4798a1bc003affe19a3232555c88eebc2e876
ravikiranyenala <yravikiranece@gmail.com>
2020-08-30 04:47:29 +0530

    added registration form, after submit the details will print in php file

---
+++ reg_getDetails.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<title> Ravi web</title>
+<br>
+<body>
+<h2>Wel-come to Student's Details page</h2>
+<br><br><br>
+
+<p>You have entered below details:<br>
+
+<?php
+	echo $_GET["sName"];
+	echo $_GET["mNum"];
+	echo $_GET["echo $_GET["mNum"];"];
+?>
+
+
+</body>
+</html>
+
